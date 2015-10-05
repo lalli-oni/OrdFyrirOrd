6c74e4a6e591ccd1db6bd4387c904b839336b2ab
lalli-oni <lalli.oni@gmail.com>
2015-10-05 14:06:04 +0200

    Removed SQL Files folder.

--- SQL Files/dbo.Table_4.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Others]
-(
-	[OtherId] INT NOT NULL PRIMARY KEY, 
-    [Other] NCHAR(10) NULL, 
-    [Token] NCHAR(10) NULL, 
-    [OthersToWords] INT NULL, 
-    CONSTRAINT [FK_Others_Words] FOREIGN KEY ([OthersToWords]) REFERENCES [Words]([WordId])
-)
--- SQL Files/dbo.Words.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Words] (
-    [WordId] INT           IDENTITY (1, 1) NOT NULL,
-    [Word]   NVARCHAR (50) NOT NULL,
-    [Token]  NCHAR (10)    NULL,
-    PRIMARY KEY CLUSTERED ([WordId] ASC)
-);
-
