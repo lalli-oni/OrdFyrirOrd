e1c365f28e206ff575879d5988b4cc092c905491
lalli-oni <lalli.oni@gmail.com>
2015-10-06 17:03:24 +0200

    Forgot to add files to staging ^^.

---
+++ OrdFyrirOrd/SourceHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace OrdFyrirOrd
+{
+    class SourceHandler
+    {
+        public string formatXml(XmlReader source, Source sourceType)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (sourceType == Source.Ordtidni)
+            {
+                while (source.Read())
+                {
+                    try
+                    {
+                        if (source.HasValue)
+                        {
+                            if (source.Value.Length > 0)
+                            {
+                                sb.AppendLine(source.Value);
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        source.Skip();
+                    }
+                }
+            }
+            else
+            {
+                throw new NotImplementedException("This source type is not supported by the SourceHandler. [Oni]");
+            }
+            return sb.ToString();
+        }
+    }
+}
